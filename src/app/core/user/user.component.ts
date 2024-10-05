1cecc3066816c5048b4176962fe2632cc551a41f
RonEerdekens <roneerdekens@gmail.com>
2024-10-05 21:36:59 +0200

    Add button
    Add a button with a event

---
+++ src/app/core/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-user';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+  get imagePath(): string {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+}
