a8fbcae9aa6c82850377e34f0cc1d5be83d75a95
RonEerdekens <roneerdekens@gmail.com>
2024-10-06 10:00:50 +0200

    Add tasks to users
    Add tasklist to users

--- src/app/core/tasks/tasks.component.ts
+++ src/app/core/tasks/tasks.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
+import { dummyTasks } from '../../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent, TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() name: string | undefined;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
   // @Input() name?: string ;
+  tasks = dummyTasks;
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.id);
+  }
 }
