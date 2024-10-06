b32783ba9b56446ae719ca34614bf7fc2c4e7fd0
RonEerdekens <roneerdekens@gmail.com>
2024-10-06 12:43:04 +0200

    Adding Pipes
    Adding a DatePipe to a date property.

--- src/app/core/task/task.component.ts
+++ src/app/core/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
+import { DatePipe } from '@angular/common';
 import { CardComponent } from '../shared/card/card.component';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
