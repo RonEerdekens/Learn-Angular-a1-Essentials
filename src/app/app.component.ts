db2a8363370d23cf04ca9e96d7645c28c7f20668
RonEerdekens <roneerdekens@gmail.com>
2024-10-06 09:37:07 +0200

    Add for and if statements
    Adding for loops and if conditionals.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import { TasksComponent } from './core/tasks/tasks.component';
 export class AppComponent {
   title = 'Essentials';
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
   }
